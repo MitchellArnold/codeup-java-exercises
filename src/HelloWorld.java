97afad7dbf0ff3e5f4832bde985f8f4b7585cd5f
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-09-16 14:32:55 -0500

    add test changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args){
-        System.out.print("Hello, world!");
+        System.out.println("Hello, world!");
         System.out.print("Hello, world!");
 
     }
