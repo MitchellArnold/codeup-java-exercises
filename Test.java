c69d3b7e07eb08fe6e6e3c7cf5f3c94b5ecca07d
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-09-18 22:07:24 -0500

    add tests

--- Test.java
+++ Test.java
@@ -1,4 +1,11 @@
 public class Test {
 
-    public
+    public static void main(String[] args){
+        String name = "codeup";
+        String cohort = "Ceres";
+
+        System.out.print(name);
+        System.out.format("Howdy, %s and welcome back to %s. \n", cohort, name);
+
+    }
 }
