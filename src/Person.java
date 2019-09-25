83d142dd98f7d53c2ab80bde338aa71f706a9911
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-09-25 11:54:53 -0500

    add changes

--- src/Person.java
+++ src/Person.java
@@ -2,27 +2,93 @@ public class Person {
     ///properties
     private String name;
 
-    public Person(){
-        System.out.println("New person");
-    }
-
-    ///returns name
-    public Person(String userName){
-        name = userName;
-    }
-    ///changes the original name
-    public String setName(String name){
-        this.name = name;
+    public String getName() {
         return name;
     }
-    ///formats a sentence using current values ( original / copies )
-    public String sayHello() {
-        return String.format("Greetings, %s!", name);
+
+    public void setName(String name) {
+        this.name = name;
     }
+
+    public void sayHello() {
+        System.out.println("Hello \n " + name);
+    }
+
+    public Person(String userName){
+        name = userName;
+//        setName(userName);   /// this is recursion example from walkthrough
+    }
+
+
 
 
     public static void main(String[] args) {
-        Person user1 = new Person("Mitchell");
-        System.out.println(user1.sayHello());
+//        Person Mitch = new Person("Mitchell");
+//        System.out.println(Mitch.getName());
+//        Mitch.setName("Arnold");
+//        Mitch.sayHello();
+
+//
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName())); /// working with only value
+//        System.out.println(person1 == person2); /// working with constructor name
+
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2); /// true; being that it's the same instance and not a new one
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1; /// same instance  **think line by line**
+//        System.out.println(person1.getName()); // John
+//        System.out.println(person2.getName()); // John
+//        person2.setName("Jane"); // when you set the name property, you are still using the same instance
+//        System.out.println(person1.getName()); // Jane (not John**)
+//        System.out.println(person2.getName()); // Jane
+
+
     }
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ///// First Run //////
+//
+//    public Person(){
+//        System.out.println("New person");
+//    }
+
+    ///returns name
+//    public Person(String userName){
+//        name = userName;
+//    }
+//    ///changes the original name
+//    public String setName(String name){
+//        this.name = name;
+//        return name;
+//    }
+//    ///formats a sentence using current values ( original / copies )
+//    public String sayHello() {
+//        return String.format("Greetings, %s!", name);
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        Person user1 = new Person("Mitchell");
+//        System.out.println(user1.sayHello());
+//    }
--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,62 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public String getString() {
+        System.out.println("Enter a string");
+        return scanner.nextLine();
+    }
+
+    public boolean yesNo() {
+        System.out.println("Is it yes or no?");
+        String answer = scanner.nextLine();
+        if(answer.equalsIgnoreCase("yes") || answer.equalsIgnoreCase("no")){
+            return true;
+        }else {
+            return false;
+        }
+
+//        return answer.equalsIgnoreCase("yes") || answer.equalsIgnoreCase("no");
+
+
+        }
+
+    public int getInt(int min, int max) {
+        String.format("Enter a number between %s and %s", min, max);
+        int userNum = scanner.nextInt();
+        if(userNum > min && userNum < max) {
+            return userNum;
+        }else {
+            return 0;
+        }
+    }
+
+    public int getInt() {
+        return scanner.nextInt();
+    }
+
+
+    public double getDouble(double min, double max){
+        String.format("Enter a number between %s and %s", min, max);
+        int userNum = scanner.nextInt();
+        if(userNum > min && userNum < max) {
+            return userNum;
+        }else {
+            return 0;
+        }
+    }
+
+    public double getDouble() {
+        return scanner.nextInt();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
 }
