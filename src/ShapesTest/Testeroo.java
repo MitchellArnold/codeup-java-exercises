1f9a491d0ac33791907ef733cbf631d0b7e7a834
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-12-01 12:21:38 -0600

    add testeroo example

---
+++ src/ShapesTest/Testeroo.java
@@ -0,0 +1,14 @@
+package ShapesTest;
+
+public class Testeroo {
+
+    private static String letter_one = "justin";
+    int number_one = 15;
+    int number_two = 20;
+
+
+    public static void main(String[] args) {
+        System.out.println("letter_one = " + letter_one);
+    }
+
+}
---
+++ src/ShapesTest/javascript.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+let x = 0;
+let y = 1;
+
+
+if(x == 0){console.log("Pour drink");}
+
+console.log(x + y);
