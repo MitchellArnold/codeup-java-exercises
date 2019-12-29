15c6a852238f87af9cd7aee2269fa856b240d856
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-12-29 11:09:25 -0600

    testeroo prac

--- src/ShapesTest/Testeroo.java
+++ src/ShapesTest/Testeroo.java
@@ -2,13 +2,23 @@ package ShapesTest;
 
 public class Testeroo {
 
-    private static String letter_one = "justin";
-    int number_one = 15;
-    int number_two = 20;
+    private static String name_one = "justin";
+    static int number_one = 15;
+    static int number_two = 20;
 
 
+    public static String sentence(){
+        return name_one + number_one;
+    }
+
+
+    
     public static void main(String[] args) {
-        System.out.println("letter_one = " + letter_one);
+        System.out.println("number_one = " + number_one);
+        System.out.println("number_two = " + number_two);
+        System.out.println("letter_one = " + name_one);
+        System.out.println("number_one + number_two = " + number_one + number_two);
+        System.out.println("sentence() = " + sentence());
     }
 
 }
